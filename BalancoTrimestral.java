5ba6349a219ce3492e62cb201778863e42602fab
Gabriela Pellin Perosa <gpp260203@gmail.com>
2021-08-19 18:25:45 +0000

    criação das variaveis

--- BalancoTrimestral.java
+++ BalancoTrimestral.java
@@ -1,4 +1,10 @@
 public class BalnacoTrimestral{
+    public static void main (String args[]){
+        int gastosJaneiro = 15000.00;
+        int gastosFevereiro=23000.00;
+        int gastosMarco = 17000.00;
+        int gastoTrimestral = gastosJaneiro + gastosFevereiro + gastosMarco;
+        int mediaTrimestral = gastoTrimestral/3;
 
-    
+    }
 }
