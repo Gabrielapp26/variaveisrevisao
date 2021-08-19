efb8de0956cd26e23ff93fd5af6b558e52aa17c8
Gabriela Pellin Perosa <gpp260203@gmail.com>
2021-08-19 18:28:21 +0000

    editando o nome da classe

--- BalancoTrimestral.java
+++ BalancoTrimestral.java
@@ -1,4 +1,4 @@
-public class BalnacoTrimestral{
+public class BalancoTrimestral{
     public static void main (String args[]){
         int gastosJaneiro = 15000.00;
         int gastosFevereiro=23000.00;
