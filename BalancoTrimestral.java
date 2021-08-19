d131f9bbdba362cf426dc3185c70b475a3ee8860
Gabriela Pellin Perosa <gpp260203@gmail.com>
2021-08-19 18:30:20 +0000

    mudança das variaveis e run no código

--- BalancoTrimestral.java
+++ BalancoTrimestral.java
@@ -1,10 +1,10 @@
 public class BalancoTrimestral{
     public static void main (String args[]){
-        int gastosJaneiro = 15000.00;
-        int gastosFevereiro=23000.00;
-        int gastosMarco = 17000.00;
-        int gastoTrimestral = gastosJaneiro + gastosFevereiro + gastosMarco;
-        int mediaTrimestral = gastoTrimestral/3;
+        double gastosJaneiro = 15000.00;
+        double gastosFevereiro=23000.00;
+        double gastosMarco = 17000.00;
+        double gastoTrimestral = gastosJaneiro + gastosFevereiro + gastosMarco;
+        double mediaTrimestral = gastoTrimestral/3;
 
         System.out.println("O gasto Trimestral é de R$ "+gastoTrimestral);
         System.out.println("A média Trimestral foi de R$ "+mediaTrimestral);
